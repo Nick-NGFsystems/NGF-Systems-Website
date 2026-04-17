274dff433237e6c7a6bd8fd321fb965a92dbdd43
Nick-NGFsystems <nick@ngfsystems.com>
2026-04-17 19:02:52 +0000

    fix: correct GA4 template literals in layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -121,11 +121,16 @@ export default function RootLayout({
       </head>
       <body className={`${sora.variable} ${inter.variable} antialiased`}>
         <Script
-          src={\}
+          src={`https://www.googletagmanager.com/gtag/js?id=${GA_MEASUREMENT_ID}`}
           strategy="afterInteractive"
         />
         <Script id="ga4-init" strategy="afterInteractive">
-          {\}
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag("js", new Date());
+            gtag("config", "${GA_MEASUREMENT_ID}");
+          `}
         </Script>
         <NgfEditBridge />
         <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
